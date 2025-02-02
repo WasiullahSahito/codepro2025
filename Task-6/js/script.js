3a3a444351f8acef2288d1d01be5c41d36a0035d
Wasiullah Sehto <131906072+WasiullahSahito@users.noreply.github.com>
2025-02-02 14:47:30 +0500

    Add files via upload

---
+++ Task-6/js/script.js
@@ -0,0 +1,10 @@
+
+        // Add scroll event listener to change navbar background
+        window.addEventListener('scroll', function() {
+            const navbar = document.querySelector('.navbar');
+            if (window.scrollY > 50) {
+                navbar.classList.add('navbar-scrolled');
+            } else {
+                navbar.classList.remove('navbar-scrolled');
+            }
+        });
